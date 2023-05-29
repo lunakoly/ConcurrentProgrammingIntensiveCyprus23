6eb79d4ce3ea498435834aaddd0555512c437980
Nikolay Lunyak <lunyak.kolya@mail.ru>
2023-05-29 21:02:07 +0300

    Solve task 3: Treiber stack

--- src/day1/TreiberStack.kt
+++ src/day1/TreiberStack.kt
@@ -10,18 +10,32 @@ class TreiberStack<E> : Stack<E> {
         // TODO: Make me linearizable!
         // TODO: Update `top` via Compare-and-Set,
         // TODO: restarting the operation on CAS failure.
-        val curTop = top.value
-        val newTop = Node(element, curTop)
-        top.value = newTop
+        while (true) {
+            val curTop = top.value
+            // (1) top may have changed
+            // (2) someone may have updated top after we acquired top.value,
+            // so the new top now has a new top.value, so we shouldn't modify
+            // the old top.value
+            // (3) top.value should always seem null to any CAS
+            // (4) the saved curTop, thought, will have changed to a non-null value
+            val newTop = Node(element, curTop)
+            if (top.compareAndSet(curTop, newTop)) {
+                return
+            }
+        }
     }
 
     override fun pop(): E? {
         // TODO: Make me linearizable!
         // TODO: Update `top` via Compare-and-Set,
         // TODO: restarting the operation on CAS failure.
-        val curTop = top.value ?: return null
-        top.value = curTop.next.value
-        return curTop.element
+        while (true) {
+            val curTop = top.value ?: return null
+            val newTop = curTop.next.value
+            if (top.compareAndSet(curTop, newTop)) {
+                return curTop.element
+            }
+        }
     }
 
     private class Node<E>(
