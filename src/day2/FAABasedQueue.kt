5a4c8c0fd8e3898cabdb3427cde675f3e58a360c
Nikolay Lunyak <lunyak.kolya@mail.ru>
2023-05-31 11:38:10 +0300

    Post-Solve task 7: FAA-based queue
    This is just a refactoring: upon entering
    the functions we already know if we should
    start from the head or the tail without
    additional comparisons.

--- src/day2/FAABasedQueue.kt
+++ src/day2/FAABasedQueue.kt
@@ -4,8 +4,8 @@ import day1.*
 import kotlinx.atomicfu.*
 
 class InfiniteArrayOfNulls<E> {
-    private val segmentHead: AtomicRef<Segment>
-    private val segmentTail: AtomicRef<Segment>
+    val segmentHead: AtomicRef<Segment>
+    val segmentTail: AtomicRef<Segment>
 
     init {
         val firstSegment = Segment(0)
@@ -13,17 +13,10 @@ class InfiniteArrayOfNulls<E> {
         segmentTail = atomic(firstSegment)
     }
 
-    fun getSegmentByIndex(index: Int): Pair<Segment, Int> {
-        val currentHead = segmentHead.value
-        val currentTail = segmentTail.value
-
+    fun getSegmentByIndex(start: Segment, index: Int): Pair<Segment, Int> {
         val segmentIndex = index / SEGMENT_SIZE
         val localIndex = index % SEGMENT_SIZE
-
-        var current = when {
-            currentTail.index <= segmentIndex -> currentTail
-            else -> currentHead
-        }
+        var current = start
 
         while (current.index < segmentIndex) {
             val maybeNextSegment = Segment(current.index + 1)
@@ -75,8 +68,9 @@ class FAABasedQueue<E> : Queue<E> {
 
     override fun enqueue(element: E) {
         while (true) {
+            val currentTail = infiniteArray.segmentTail.value
             val i = enqIdx.getAndIncrement()
-            val (segment, index) = infiniteArray.getSegmentByIndex(i)
+            val (segment, index) = infiniteArray.getSegmentByIndex(currentTail, i)
 
             if (segment.data[index].compareAndSet(null, element)) {
                 if (index == SEGMENT_SIZE - 1) {
@@ -92,8 +86,9 @@ class FAABasedQueue<E> : Queue<E> {
     override fun dequeue(): E? {
         while (true) {
             if (deqIdx.value >= enqIdx.value) return null
+            val currentHead = infiniteArray.segmentHead.value
             val i = deqIdx.getAndIncrement()
-            val (segment, index) = infiniteArray.getSegmentByIndex(i)
+            val (segment, index) = infiniteArray.getSegmentByIndex(currentHead, i)
 
             if (segment.data[index].compareAndSet(null, POISONED)) {
                 if (index == SEGMENT_SIZE - 1) {
