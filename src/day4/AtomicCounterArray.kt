d93fd24f30712c3b2a20810bc0ab5e67ef75aa5d
Nikolay Lunyak <lunyak.kolya@mail.ru>
2023-06-01 21:26:27 +0300

    Solve task 11: Array of Atomic Counters
    ***Thanks to Stas***

--- src/day4/AtomicCounterArray.kt
+++ src/day4/AtomicCounterArray.kt
@@ -7,7 +7,7 @@ import kotlinx.atomicfu.*
 
 // This implementation never stores `null` values.
 class AtomicCounterArray(size: Int) {
-    private val array = atomicArrayOfNulls<Int>(size)
+    private val array = atomicArrayOfNulls<Any>(size)
 
     init {
         // Fill array with zeros.
@@ -16,17 +16,35 @@ class AtomicCounterArray(size: Int) {
         }
     }
 
-    fun get(index: Int): Int {
-        // TODO: the cell can store a descriptor.
-        return array[index].value!!
+    // TODO: the cell can store a descriptor.
+    fun get(index: Int): Int = when (val value = array[index].value) {
+        is IncrementDescriptor -> when (value.status.value) {
+            SUCCESS -> value.getValueAfterIncrementFor(index)
+            else -> value.getValueBeforeIncrementFor(index)
+        }
+        else -> value as? Int ?: error("Invalid element type: $value")
     }
 
-    fun inc2(index1: Int, index2: Int) {
-        require(index1 != index2) { "The indices should be different" }
+    fun inc2(indexA: Int, indexB: Int) {
+        require(indexA != indexB) { "The indices should be different" }
         // TODO: This implementation is not linearizable!
         // TODO: Use `IncrementDescriptor` to perform the operation atomically.
-        array[index1].value = array[index1].value!! + 1
-        array[index2].value = array[index2].value!! + 1
+
+        val index1 = if (indexA <= indexB) indexA else indexB
+        val index2 = if (indexA <= indexB) indexB else indexA
+
+        while (true) {
+            val value1 = get(index1)
+            val value2 = get(index2)
+
+            val descriptor = IncrementDescriptor(index1, value1, index2, value2).also {
+                it.applyOperation()
+            }
+
+            if (descriptor.status.value == SUCCESS) {
+                break
+            }
+        }
     }
 
     // TODO: Implement the `inc2` operation using this descriptor.
@@ -40,11 +58,57 @@ class AtomicCounterArray(size: Int) {
     ) {
         val status = atomic(UNDECIDED)
 
+        init {
+            require(index1 <= index2) { "index1 <= index2 violated: $index1, $index2" }
+        }
+
+        fun getValueBeforeIncrementFor(index: Int) = when (index) {
+            index1 -> valueBeforeIncrement1
+            index2 -> valueBeforeIncrement2
+            else -> error("Index $index is not present in the descriptor residing in this location")
+        }
+
+        fun getValueAfterIncrementFor(index: Int) = getValueBeforeIncrementFor(index) + 1
+
+        fun canOccupyCell(index: Int, valueBeforeIncrement: Int): Boolean {
+            array[index].compareAndSet(valueBeforeIncrement, this)
+
+            when (val value = array[index].value) {
+                this -> return true
+                is IncrementDescriptor -> value.applyOperation()
+                valueBeforeIncrement -> {} // Didn't manage to do it in time, fuck it then
+                else -> status.compareAndSet(UNDECIDED, FAILED)
+            }
+
+            return false
+        }
+
         // TODO: Other threads can call this function
         // TODO: to help completing the operation.
         fun applyOperation() {
             // TODO: Use the CAS2 algorithm, installing this descriptor
             // TODO: in `array[index1]` and `array[index2]` cells.
+
+            while (status.value == UNDECIDED) {
+                if (!canOccupyCell(index1, valueBeforeIncrement1)) {
+                    continue
+                }
+
+                if (!canOccupyCell(index2, valueBeforeIncrement2)) {
+                    continue
+                }
+
+                status.compareAndSet(UNDECIDED, SUCCESS)
+            }
+
+            val valueShift = when (status.value) {
+                SUCCESS -> 1
+                FAILED -> 0
+                else -> error("Should have no come this far")
+            }
+
+            array[index1].compareAndSet(this, valueBeforeIncrement1 + valueShift)
+            array[index2].compareAndSet(this, valueBeforeIncrement2 + valueShift)
         }
     }
 
